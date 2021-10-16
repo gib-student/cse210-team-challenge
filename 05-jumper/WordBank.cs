f0de8783a43c1e5317c24b8d8518d135654a4e40
LindseyK2 <lindseyk2@byui.edu>
2021-10-15 20:01:55 -0600

    still getting the char to string error

--- 05-jumper/WordBank.cs
+++ 05-jumper/WordBank.cs
@@ -8,35 +8,32 @@ namespace _05_jumper
 {
     public class WordBank
     {
-        public int _number;
         public string _urlText;
         public string _word;
         public List<char> _randomWord = new List<char>();
-        // public int RandomNumber()
-        // {
-        //     Random randomGenerator = new Random();
-        //     _number = randomGenerator.Next(0,1000);
-        //     return _number;
-        // }
         public string Callurl()  
         {  
-            // WebRequest request = HttpWebRequest.Create("https://www.mit.edu/~ecprice/wordlist.10000");  
-            // WebResponse response = request.GetResponse();  
-            // StreamReader reader = new StreamReader(response.GetResponseStream());  
-            // string _urlText = reader.ReadToEnd(); // it takes the response from your url. now you can use as your need  
-            // return _urlText;
-
             using(WebClient client = new WebClient())
             {
                 _urlText = client.DownloadString("https://www.mit.edu/~ecprice/wordlist.10000");
                 return _urlText;
             }
-        }  
+        } 
+
+        public List<char> CreateList()
+        {
+            foreach (char c in _urlText)
+            {
+                _randomWord.Add(c);
+            }
+            return _randomWord;
+        }
+        
         public string RandomWord()
         {
-            Random randomWord = new Random();
-            int i = randomWord.Next(0,5);
-            _word = _urlText[i].ToString();
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(0, 1000);
+            _word = _randomWord[number];
             return _word;
         }
     }
