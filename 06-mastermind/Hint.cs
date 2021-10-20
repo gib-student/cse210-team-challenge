e31eeebc5e57b2ffa02f9907f2f37daa365f8a92
Ismael Moises Tenango Anaya <ten20007@byui.edu>
2021-10-20 13:03:16 -0600

    ismael commit

--- 06-mastermind/Hint.cs
+++ 06-mastermind/Hint.cs
@@ -4,9 +4,30 @@ namespace _06_mastermind
 {
     public class Hint
     {
-        static void Main(string[] args)
+        // circule menas the number is correct but not in the right place
+        // Exes means correct number in the corret position
+        // asterict means 
+        private void compare_guess()
         {
-            Console.WriteLine("Starting point for the Mastermind project.");
+            bool compare_board = false;
+            foreach(int user_random_num in _random_num)
+            {
+                if (user_random_num == _random_num)
+                {
+                    compare_board = true
+                }
+                else if (user_random_num != _random_num)
+                {
+                    compare_board = false
+                }
+            }
+            return !compare_board;
+        }
+        public get_hint()
+        {
+            string asterisk = "****";
+
+
         }
     }
 }
