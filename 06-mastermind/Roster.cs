62e893d08db335f132cba8c25b5f29ce61692be9
MichaeJ0131 <91762833+MichaeJ0131@users.noreply.github.com>
2021-10-22 18:42:17 -0600

    Update Roster.cs
    This is what I am thinking so far for the roster. Let me know if you think I could add or do something different

--- 06-mastermind/Roster.cs
+++ 06-mastermind/Roster.cs
@@ -4,9 +4,29 @@ namespace _06_mastermind
 {
     public class Roster
     {
-        static void Main(string[] args)
+        private int _currentPlayerIndex = 0;
+        private List<Player> _players = new List<Player>();
+        
+        public void AddPlayer(Player player)
         {
-            Console.WriteLine("Starting point for the Mastermind project.");
+            _players.Add(player);
         }
-    }
+        public player GetCurrentPlayer()
+        {
+            return _players[_currentPlayerIndex];
+        }
+        public void AdvanceNextPlayer()
+        {
+            _currentPlayerIndex = (_currentPlayerIndex + 1) % _players.Count;
+        }
+        
+
+//         Console.WriteLine("Enter Player One's Name: ");
+//         string player_one = Console.ReadLine();
+//         Console.WriteLine("Enter Player Two's Name: ");
+//         string player_two = Console.ReadLine();
+//         Console.WriteLine($"Welcome, {player_one}");
+//         Console.WriteLine($"Welcome, {player_two}");
+        
+      }
 }
