0a393048d532bde8a36a9b5f611277db07fe0302
Ismael Moises Tenango Anaya <ten20007@byui.edu>
2021-10-23 13:58:25 -0600

    push2

--- 06-mastermind/Director.cs
+++ 06-mastermind/Director.cs
@@ -58,7 +58,7 @@ namespace _06_mastermind
         {   
             //Apply the move from Current Player to the Board
             string currentPlayer = _roster.GetCurrentPlayer();
-            string hint = _hint.GetHint();
+            string hint = _hint.GetHint({_board.GetSecretNum(currentPlayer());
             _board.Apply(currentPlayer, _guess, hint);
         }
         
