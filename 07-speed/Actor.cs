30d4f1bd1c443062f4ad851e83ef8e9a1730bb2a
LindseyK2 <lindseyk2@byui.edu>
2021-10-27 12:31:08 -0600

    Updated the constant and actor class

--- 07-speed/Actor.cs
+++ 07-speed/Actor.cs
@@ -2,8 +2,13 @@ using System;
 
 namespace _07_speed
 {
-    class Actor
+    public class Actor
     {
+        protected Point _position;
+        protected Point _velocity;
 
+        protected int _fontSize = Constant.DEFAULT_FONT_SIZE;
+
+        
     }
 }
--- 07-speed/Constant.cs
+++ 07-speed/Constant.cs
@@ -2,8 +2,14 @@ using System;
 
 namespace _07_speed
 {
-    class Constant
+    public static class Constant
     {
+        public const int MAX_X = 600;
+        public const int MAX_Y = 400;
+        public const int FRAME_RATE = 45;
 
+        public const int DEFAULT_FONT_SIZE = 18;
+        public const int MAX_WORD_SPEED = 5;
+        public const double RANDOM_WORD_RATE = 0.02;
     }
 }
