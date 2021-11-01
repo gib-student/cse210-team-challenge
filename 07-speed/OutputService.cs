7945ab2fc30b79baa4c0b4cffd4f3fd710a0286b
Ismael Moises Tenango Anaya <ten20007@byui.edu>
2021-11-01 12:15:19 -0600

    hi

--- 07-speed/OutputService.cs
+++ 07-speed/OutputService.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Collections.Generic;
+using Raylib_cs;
 
 namespace _07_speed
 {
     class OutputService
     {
+        private Raylib_cs.Color _backgroundColor = Raylib_cs.Color.WHITE;
+
+        public OutputService()
+        {
+
+        }
 
     }
 }
