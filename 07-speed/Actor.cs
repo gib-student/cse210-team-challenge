8f8210e212b2def1ba5a05981f109996d5d6c344
LindseyK2 <lindseyk2@byui.edu>
2021-11-01 12:17:42 -0600

    Test2

--- 07-speed/Actor.cs
+++ 07-speed/Actor.cs
@@ -8,7 +8,75 @@ namespace _07_speed
         protected Point _velocity;
 
         protected int _fontSize = Constant.DEFAULT_FONT_SIZE;
+        protected string _text = "";
 
+        public Actor()
+        {
+
+        }
+
+        public bool HasText()
+        {
+            return _text != "";
+        }
+
+        public string GetText()
+        {
+            return _text;
+        }
+
+        public int GetX()
+        {
+            return _position.GetX();
+        }
+
+        public int GetY()
+        {
+            return _position.GetY();
+        }
+
+        public int GetFontSize()
+        {
+            return _fontSize;
+        }
+
+        public Point GetPosition()
+        {
+            return _position;
+        }
+
+        public Point GetVelocity()
+        {
+            return _velocity;
+        }
         
+        public void SetVelocity(Point newVelocity)
+        {
+            _velocity = newVelocity;
+        }
+
+        public void MoveNext()
+        {
+            int x = _position.GetX();
+            int y = _position.GetY();
+
+            int dx = _velocity.GetX();
+            int dy = _velocity.GetY();
+
+            int newX = (x + dx) % Constant.MAX_X;
+            int newY = (y + dy) % Constant.MAX_Y;
+
+            if (newX < 0)
+            {
+                newX = Constant.MAX_X;
+            }
+
+            if (newY < 0)
+            {
+                newY = Constant.MAX_Y;
+            }
+
+            _position = new Point(newX, newY);
+        }
     }
 }
