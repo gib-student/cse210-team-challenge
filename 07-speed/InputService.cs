2dacb7c301c85f9e38cb76d5b253d93856f82e83
Ismael Moises Tenango Anaya <ten20007@byui.edu>
2021-11-02 10:45:54 -0600

    input added some errors thou

--- 07-speed/InputService.cs
+++ 07-speed/InputService.cs
@@ -1,14 +1,37 @@
 using System;
+using System.Collections.Generic;
+using Raylib_cs;
 
 namespace _07_speed
 {
     /// What is the purpose of this class?
     class InputService
     {
+        
+    }
+     public string GetLetter()
+    {
+        int KeyInt = Raylib.GetKeyPressed();
+
+        {int KeyString  = "";
+
+            if (KeyInt  != 0)
+            {
+                if ((Raylib_cs.KeyboardKey)keyInt == Raylib_cs.KeyboardKey.KEY_ENTER)
+                {
+                    keyString = "/n";
+                }
+                else
+                {
+                    char keyChar = (char)KeyInt;
+                    keyString = keyChar.ToString().ToLower();
+                }
+            }
+            return KeyString;
+        }
 
     }
 }
-
 
 // for each word w in _words)
 // {
