10e9567de3d69f0c2adf2395eda6d41d8b2dbafc
gib-student <gib19021@byui.edu>
2021-11-03 12:41:36 -0600

    Update Buffer.cs

--- 07-speed/Buffer.cs
+++ 07-speed/Buffer.cs
@@ -2,8 +2,30 @@ using System;
 
 namespace _07_speed
 {
+    /// Store the data which will be in the buffer on the bottom-left of the
+    /// screen. 
     class Buffer : Actor
     {
+        private Point _bufferPoint;
 
+        public Buffer()
+        {
+            _position = new Point(599, 0);
+        }
+
+        public void AddLetter(char letter)
+        {
+            _buffer += letter;
+        }
+
+        public override string ToString()
+        {
+            return _buffer;
+        }
+
+        public void Clear()
+        {
+            _buffer = "";
+        }
     }
 }
